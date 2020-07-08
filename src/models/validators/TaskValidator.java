941ea57d4d914b9dc45b6487133888c959e9785d
iroha <yuuteiyuu@gmail.com>
2020-07-08 16:42:02 +0900

    Add validation

---
+++ src/models/validators/TaskValidator.java
@@ -0,0 +1,41 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class TaskValidator {
+    public static List<String> validate(Task m) {
+        List<String> errors = new ArrayList<String>();
+
+        String title_error = _validateTitle(m.getTitle());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
+
+        String content_error = _validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    private static String _validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タイトルを入力してください。";
+        }
+
+        return "";
+    }
+
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "メッセージを入力してください。";
+        }
+
+        return "";
+    }
+
+}
